b5f0a4246c0f0457c17ede8c583105ea0c46d96c
hasibmuhammad <hasibmu99@gmail.com>
2023-10-28 07:06:24 +0600

    Counter added

--- src/components/Header/Favorite/Favorite.jsx
+++ src/components/Header/Favorite/Favorite.jsx
@@ -3,7 +3,10 @@ import { MdFavoriteBorder } from "react-icons/md";
 
 const Favorite = () => {
   return (
-    <div className="text-4xl">
+    <div className="text-4xl relative">
+      <span className="text-sm w-5 h-5 text-center bg-orange-400 rounded-full font-bold absolute -right-1 top-4 flex justify-center items-center">
+        1
+      </span>
       <MdFavoriteBorder />
     </div>
   );
--- src/components/Header/Wishlist/Wishlist.jsx
+++ src/components/Header/Wishlist/Wishlist.jsx
@@ -3,7 +3,10 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 
 const Wishlist = () => {
   return (
-    <div className="text-4xl">
+    <div className="text-4xl relative">
+      <span className="text-sm w-5 h-5 text-center bg-orange-400 rounded-full font-bold absolute -right-1 top-4 flex justify-center items-center">
+        0
+      </span>
       <AiOutlineShoppingCart />
     </div>
   );
