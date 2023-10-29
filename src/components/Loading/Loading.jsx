ef127630713956e2634c0554c9708ee39f46f863
hasibmuhammad <hasibmu99@gmail.com>
2023-10-29 20:55:04 +0600

    Loader - Added

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -3,7 +3,11 @@ const Loading = ({ isLoading }) => {
     <>
       {isLoading && (
         <div className="text-center mt-72">
-          <p className="mx-4">Loading...</p>
+          {/* <p className="mx-4">Loading...</p> */}
+          <span className="loading loading-ball loading-xs"></span>
+          <span className="loading loading-ball loading-sm"></span>
+          <span className="loading loading-ball loading-md"></span>
+          <span className="loading loading-ball loading-lg"></span>
         </div>
       )}
     </>
