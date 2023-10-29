f436fb80ac5ce35e679e9497aece93cc861a9ef3
hasibmuhammad <hasibmu99@gmail.com>
2023-10-29 20:48:35 +0600

    Condition - Modified

--- src/components/Games/Games.jsx
+++ src/components/Games/Games.jsx
@@ -14,63 +14,68 @@ const Games = ({
   const [listView, setListView] = useState(false);
 
   return (
-    <div className="my-8 mx-4">
-      <div className="mb-4 flex justify-between items-center">
-        <div>
-          <h1 className="text-7xl font-bold">New and trending</h1>
-          <p className="ml-1">VAWG Video Games</p>
-        </div>
-        <div className="flex justify-normal gap-2 items-center text-2xl">
-          <button
-            title="Grid View"
-            onClick={() => setListView(false)}
-            className={
-              !listView && "bg-orange-400 font-bold p-1 rounded-md text-black"
-            }
-          >
-            <BsFillGrid3X3GapFill />
-          </button>
-          <button
-            title="List View"
-            onClick={() => setListView(true)}
-            className={
-              listView && "bg-orange-400 font-bold p-1 rounded-md text-black"
-            }
-          >
-            <BsViewList />
-          </button>
-        </div>
-      </div>
-      <div
-        className={`grid gap-4 ${
-          listView
-            ? "grid-cols-1 md:grid-cols-1 lg:grid-cols-1"
-            : "grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
-        }`}
-      >
-        {games.map((gm) => (
-          <Game key={gm.id} game={gm} handleFavorite={handleFavorite} />
-        ))}
-      </div>
+    <>
       {!isLoading && (
-        <div className="mt-4 flex justify-between items-center">
-          <button
-            onClick={() => handlePrevious(previous)}
-            className={`btn btn-outline capitalize px-5 ${
-              !previous && "btn-disabled"
+        <div className="my-8 mx-4">
+          <div className="mb-4 flex justify-between items-center">
+            <div>
+              <h1 className="text-7xl font-bold">New and trending</h1>
+              <p className="ml-1">VAWG Video Games</p>
+            </div>
+            <div className="flex justify-normal gap-2 items-center text-2xl">
+              <button
+                title="Grid View"
+                onClick={() => setListView(false)}
+                className={
+                  !listView &&
+                  "bg-orange-400 font-bold p-1 rounded-md text-black"
+                }
+              >
+                <BsFillGrid3X3GapFill />
+              </button>
+              <button
+                title="List View"
+                onClick={() => setListView(true)}
+                className={
+                  listView &&
+                  "bg-orange-400 font-bold p-1 rounded-md text-black"
+                }
+              >
+                <BsViewList />
+              </button>
+            </div>
+          </div>
+          <div
+            className={`grid gap-4 ${
+              listView
+                ? "grid-cols-1 md:grid-cols-1 lg:grid-cols-1"
+                : "grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
             }`}
           >
-            Previous
-          </button>
-          <button
-            onClick={() => handleNext(next)}
-            className="btn btn-outline capitalize px-5"
-          >
-            Next
-          </button>
+            {games.map((gm) => (
+              <Game key={gm.id} game={gm} handleFavorite={handleFavorite} />
+            ))}
+          </div>
+
+          <div className="mt-4 flex justify-between items-center">
+            <button
+              onClick={() => handlePrevious(previous)}
+              className={`btn btn-outline capitalize px-5 ${
+                !previous && "btn-disabled"
+              }`}
+            >
+              Previous
+            </button>
+            <button
+              onClick={() => handleNext(next)}
+              className="btn btn-outline capitalize px-5"
+            >
+              Next
+            </button>
+          </div>
         </div>
       )}
-    </div>
+    </>
   );
 };
 
